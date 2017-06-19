118f50e86e1dfb4b482638dcb3b44212d66ecdcd
Jacob <jacobjc@gmail.com>
2017-06-18 22:37:42 -0700

    update

--- 1-CreateRole.sh
+++ 1-CreateRole.sh
@@ -8,6 +8,10 @@ CFFile="file://1-CustomPolicy.json"
 #
 # Create the Role
 aws iam create-role --role-name $RoleName --assume-role-policy-document $RolePolicy --description $RoleDesc
+# Now we have to create an Instance Profile for this role (match the RoleName)
+aws iam create-instance-profile --instance-profile-name $RoleName
+# Now we have to attach the Instance-profile to the Role
+aws iam add-role-to-instance-profile --role-name $RoleName --instance-profile-name $RoleName
 # Attach a custom policy to that role
 aws iam put-role-policy --role-name $RoleName --policy-name CustomPolicy --policy-document $CFFile
 # Attach the standard AWS policies
